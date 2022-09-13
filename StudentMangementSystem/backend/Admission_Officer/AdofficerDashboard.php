35f04ba6ba18dd7b1ce7e568273b002639cf9cbc
pappu2020 <64909460+pappu2020@users.noreply.github.com>
2022-09-13 16:53:05 +0600

    php Home

--- StudentMangementSystem/backend/Admission_Officer/AdofficerDashboard.php
+++ StudentMangementSystem/backend/Admission_Officer/AdofficerDashboard.php
@@ -1,4 +1,14 @@
-<?php session_start(); ?>
+<?php session_start();
+require_once('../../backend/db.php');
+$addOfficerLoginEmail  = $_SESSION["getEmpEmailSession"];
+
+$allAdOfficerData = "SELECT * FROM employees WHERE email='$addOfficerLoginEmail' AND postion='Admission_Officier'";
+$allAdOfficerDataRes = mysqli_query($con, $allAdOfficerData);
+$allAdOfficerDataFetch = mysqli_fetch_assoc($allAdOfficerDataRes);
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -122,15 +132,16 @@
                     <div class=" bd-highlight userTitleDiv">
 
                         <p class="userTitle">Welcome, <?php
-                                                        if (isset($_SESSION["getEmpnameSession"])) {
-                                                            echo  $_SESSION["getEmpnameSession"];
-                                                        }
+                                                        // if (isset($_SESSION["getEmpnameSession"])) {
+                                                        //     echo  $_SESSION["getEmpnameSession"];
+                                                        // }
+                                                        printf($allAdOfficerDataFetch["name"]);
 
                                                         ?></p1>
                         <p class="userSubTitle">Admission Officer</p>
                     </div>
                     <div class=" bd-highlight">
-                        <img src="../../backend/admin/Employee/empImage/<?php echo $_SESSION["getEmpPhotoSession"]; ?>" class="userImage rounded rounded-circle" alt="" width="60px" height="60px">
+                        <img src="../../backend/admin/Employee/empImage/<?php print_r($allAdOfficerDataFetch["photo"]) ?>" class="userImage rounded rounded-circle" alt="" width="60px" height="60px">
                     </div>
 
 
@@ -149,11 +160,63 @@
 
 
         <div class="MainSectionContainer">
-            <h1 class="mainSectionHeading">Profile</h1>
+            <h1 class="mainSectionHeading mb-3">Profile</h1>
             <div class="row mainSectionStart">
                 <div class="col-lg-3 mainSectionLeft">
 
-                    <img src="../../backend/admin/Employee/empImage/<?php echo $_SESSION["getEmpPhotoSession"]; ?>" alt="" class="mainSectionLeftImage" width="300px" height="400px">
+                    <img src="../../backend/admin/Employee/empImage/<?php print_r($allAdOfficerDataFetch["photo"]) ?>" alt="" class="mainSectionLeftImage" width="300px" height="400px">
+
+
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <span class="bd-highlight editIconPicDiv rounded rounded-circle"><svg xmlns="http://www.w3.org/2000/svg" class="editIconPic" width="24" height="24" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
+                            </svg></span>
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade AdoffModel" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Profile Pic Upload</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <form action="./AdofficerProfileUpdate.php" method="POST" enctype="multipart/form-data">
+                                    <div class="modal-body">
+
+                                        <img src="../../backend/admin/Employee/empImage/<?php print_r($allAdOfficerDataFetch["photo"]) ?>" alt="" class="adOffiCierProfilePic" width="300px" height="300px">
+                                        <input type="file" class="form-control mt-3" name="addoffProfileup">
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-secondary" name="addoffProfileupBtn">Upload</button>
+
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -162,9 +225,7 @@
 
                         <p class="mobileNumber flex-grow-1 bd-highlight"><span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
                                     <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-                                </svg></span> <?php if (isset($_SESSION["getEmpmobileSession"])) {
-                                                    echo $_SESSION["getEmpmobileSession"];
-                                                } ?></p>
+                                </svg></span> <?php printf($allAdOfficerDataFetch["phoneNum"]);  ?></p>
 
 
                         <span class="bd-highlight editIconDiv rounded rounded-circle"><svg xmlns="http://www.w3.org/2000/svg" class="editIcon" width="24" height="24" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
@@ -198,22 +259,181 @@
 
 
 
-                <div class="col-lg-3 mainSectionRight">
+                <div class="col-lg-8 mainSectionRight">
+
+                    <form action="./AdofficerProfileUpdate.php" method="POST">
+
+                        <div class="row g-3">
+                            <div class="col-lg-3">
+
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Name</label>
+                                    <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["name"]; ?>" name="addOfficerName">
+                                </div>
+
+
+                            </div>
+                            <div class="col-lg-3">
+
+                                <label for="name" class="form-label">Father Name</label>
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["fatherName"]; ?>" name="addOfficerFatherName">
+
+
+                            </div>
+                            <div class="col-lg-3">
+                                <label for="name" class="form-label">Mother Name</label>
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["motherName"]; ?>" name="addOfficerMotherName">
+
+                            </div>
+
+                            <div class="col-lg-3">
+                                <label for="name" class="form-label">National Id Number</label>
+                                <input type="number" class="form-control" value="<?= $allAdOfficerDataFetch["nationalid"]; ?>" name="addOfficerNationalId">
+
+                            </div>
+
+                            <div class="col-lg-3">
+                                <label for="name" class="form-label">Birth Reg. Number</label>
+                                <input type="number" class="form-control" value="<?= $allAdOfficerDataFetch["birthRegNum"]; ?>" name="addOfficerBirthReg">
+
+                            </div>
+
+
+
+                            <div class="col-lg-3">
+
+                                <label for="dob" class="form-label">Date Of Birth</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["dob"]; ?>" name="addOfficerDob">
+
+
+
+                            </div>
+
+
+                            <div class="col-lg-2">
+
+                                <label for="dob" class="form-label">Blood Group</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["bloodGrp"]; ?>" name="addOfficerBloodGrp">
+
+
+
+                            </div>
+
+
+                            <div class="col-lg-2">
+                                <label for="dob" class="form-label">Religion</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["religion"]; ?>" name="addOfficerReligion">
+                            </div>
+
+                            <div class="col-lg-2">
+                                <label for="dob" class="form-label">Gender</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["Gender"]; ?>" name="addOfficerGender">
+                            </div>
+
+                            <div class="col-lg-3">
+                                <label for="dob" class="form-label">How to know about us</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["Howtoknowaboutus"]; ?>" name="addOfficerHoWTo">
+                            </div>
+
+
+                            <div class="col-lg-4">
+                                <label for="dob" class="form-label">Present Address</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["presentAddress"]; ?>" name="addOfficerPresent">
+                            </div>
+
+
+                            <div class="col-lg-4">
+                                <label for="dob" class="form-label">Parmanant Address</label>
+
+                                <input type="text" class="form-control" value="<?= $allAdOfficerDataFetch["parmanentAddress"]; ?>" name="addOfficerParmanant">
+                            </div>
+
+
+                            <div class="col-lg-3 addOfficerEditDiv">
+                                <button class="addOfficerEditParaLink btn" type="submit" name="addOfficerInfoUpdateBtn">
+                                    <p class="addOfficerEditPara">Edit Profile</p>
+                                </button>
+                            </div>
+
+
+
+
+
+
+
+
+
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    </form>
+                    <?php
+                    if (isset($_SESSION["addOfficerUpdateSuccess"])) {
+
+
+                    ?>
+
+                        <div class="alert alert-success mt-5"><?= $_SESSION["addOfficerUpdateSuccess"]; ?></div>
+
+                    <?php }
+                    unset($_SESSION["addOfficerUpdateSuccess"]) ?>
+
+
+
 
                 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
+
+
+
+
+
+
+
         </div>
 
-
-
-
-
-
-
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/ecd7a02304.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/ecd7a02304.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ StudentMangementSystem/backend/Admission_Officer/AdofficerProfileUpdate.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+require_once('../../backend/db.php');
+$addOfficerLoginEmailForUpdate  = $_SESSION["getEmpEmailSession"];
+
+
+if(isset($_POST["addOfficerInfoUpdateBtn"])){
+
+$addOfficerName=$_POST["addOfficerName"];
+
+$addOfficerFatherName=$_POST["addOfficerFatherName"];
+
+$addOfficerMotherName=$_POST["addOfficerMotherName"];
+
+$addOfficerNationalId=$_POST["addOfficerNationalId"];
+
+$addOfficerBirthReg=$_POST["addOfficerBirthReg"];
+
+$addOfficerDob=$_POST["addOfficerDob"];
+
+$addOfficerBloodGrp=$_POST["addOfficerBloodGrp"];
+
+$addOfficerReligion=$_POST["addOfficerReligion"];
+
+$addOfficerGender=$_POST["addOfficerGender"];
+
+$addOfficerHoWTo=$_POST["addOfficerHoWTo"];
+
+$addOfficerPresent=$_POST["addOfficerPresent"];
+
+$addOfficerParmanant=$_POST["addOfficerParmanant"];
+
+
+
+
+$updateAdOfficer = "UPDATE employees SET name='$addOfficerName',fatherName='$addOfficerFatherName',motherName='$addOfficerMotherName',nationalid='$addOfficerNationalId',birthRegNum='$addOfficerBirthReg',dob='$addOfficerDob',bloodGrp='$addOfficerBloodGrp',religion='$addOfficerReligion',Gender='$addOfficerGender',Howtoknowaboutus='$addOfficerHoWTo',presentAddress='$addOfficerPresent',parmanentAddress='$addOfficerParmanant' WHERE email='$addOfficerLoginEmailForUpdate'";
+
+
+$updateAdOfficerRes = mysqli_query($con,$updateAdOfficer);
+
+
+if($updateAdOfficerRes){
+    $_SESSION["addOfficerUpdateSuccess"]="Update Success!!!";
+    header("location:./AdofficerDashboard.php");
+}
+
+}
+
+if(isset($_POST["addoffProfileupBtn"])){
+
+
+
+
+        $addOfficerName = $_SESSION["getEmpnameSession"];
+        
+
+        $fullPath = $_FILES["addoffProfileup"]["name"];
+        $explodePart = explode(".",$fullPath);
+        $extension = end($explodePart);
+
+        $fullName = $addOfficerName."-".time().".". $extension;
+
+        $tempPath = $_FILES["addoffProfileup"]["tmp_name"];
+        $newPath = "../../backend/admin/Employee/empImage/".$fullName;
+
+       
+
+        move_uploaded_file($tempPath,$newPath);
+
+        $updateProfileAdOfficer = "UPDATE employees SET photo='$fullName' WHERE email='$addOfficerLoginEmailForUpdate' AND postion='Admission_Officier'";
+
+        $updateProfileAdOfficerRes = mysqli_query($con,$updateProfileAdOfficer);
+
+        header("location:./AdofficerDashboard.php");
+
+
+        
+
+    
+    
+}
